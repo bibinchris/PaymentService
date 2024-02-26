07ac43cc019ddfb6ea024c9025eaf1518965d3a2
Bibin <bibin_chris@yahoo.com>
2024-02-27 01:40:10 +0530

    New Payment Service Created

--- src/main/java/com/paymentservice/controller/PaymentController.java
+++ src/main/java/com/paymentservice/controller/PaymentController.java
@@ -3,9 +3,11 @@ package com.paymentservice.controller;
 import com.paymentservice.dtos.*;
 import com.paymentservice.services.*;
 import com.razorpay.*;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/payment")
+@RestController
+@RequestMapping("/payment")
 public class PaymentController {
 
     PaymentService paymentService;
@@ -17,5 +19,11 @@ public class PaymentController {
     @PostMapping("/{order_id}")
     public String initiatePayment(@RequestBody InitiatePaymentRequestDto requestDto) throws RazorpayException {
         return paymentService.initiatePayment(requestDto);
+    }
+
+    @GetMapping("/test/{id}")
+    public ResponseEntity<String> testUserService(@PathVariable("id") Long paymentId) {
+        System.out.println("Request Received for PaymentId = " + paymentId);
+        return new ResponseEntity<>("PaymentId == "+paymentId, HttpStatus.OK);
     }
 }
